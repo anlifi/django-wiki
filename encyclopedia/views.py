c1aab5b15fb31ae8ec7ca5f4e0ab74af8345127a
Fischer, Angelina <angelina.fischer@sva.de>
2023-05-16 11:35:59 +0200

    Add search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,19 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from markdown2 import Markdown
 
 from . import util
 
 
+class SearchForm(forms.Form):
+    q = forms.CharField(label="", widget=forms.TimeInput(attrs={'placeholder': 'Search Encyclopedia'}))
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
+        "search": SearchForm(),
         "entries": util.list_entries()
     })
 
@@ -22,3 +30,21 @@ def entry(request, title):
             "title": title,
             "entry": markdowner.convert(entry)
         })
+    
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            q:str = form.cleaned_data["q"]
+            entries = util.list_entries()
+            if q in entries:
+                return HttpResponseRedirect(reverse("entry", kwargs={"title": q}))
+            else:
+                return render(request, "encyclopedia/search.html", {
+                    "results": [s for s in entries if q.lower() in s.lower()]
+                })
+    
+    return render(request, "encyclopedia/search.html", {
+        "results": []
+    })
