efb2bfc2b296df1bbd1f98cbb702bb4731a8b152
Fischer, Angelina <angelina.fischer@sva.de>
2023-05-16 10:33:21 +0200

    Add entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    else:
+        markdowner = Markdown()
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdowner.convert(entry)
+        })
