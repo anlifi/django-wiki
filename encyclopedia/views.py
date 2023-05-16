98736b47cb35042f14e1d32ab308f8491428fdca
Fischer, Angelina <angelina.fischer@sva.de>
2023-05-16 14:16:17 +0200

    Add new page functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,7 +8,12 @@ from . import util
 
 
 class SearchForm(forms.Form):
-    q = forms.CharField(label="", widget=forms.TimeInput(attrs={'placeholder': 'Search Encyclopedia'}))
+    q = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Search Encyclopedia"}))
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Title", "class": "row"}))
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={"placeholder": "Markdown Content", "rows": "5", "class": "row"}))
 
 
 def index(request):
@@ -24,12 +29,12 @@ def entry(request, title):
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
-    else:
-        markdowner = Markdown()
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "entry": markdowner.convert(entry)
-        })
+    
+    markdowner = Markdown()
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": markdowner.convert(entry)
+    })
     
 
 def search(request):
@@ -40,11 +45,32 @@ def search(request):
             entries = util.list_entries()
             if q in entries:
                 return HttpResponseRedirect(reverse("entry", kwargs={"title": q}))
-            else:
-                return render(request, "encyclopedia/search.html", {
-                    "results": [s for s in entries if q.lower() in s.lower()]
-                })
+            
+            return render(request, "encyclopedia/search.html", {
+                "results": [s for s in entries if q.lower() in s.lower()]
+            })
     
     return render(request, "encyclopedia/search.html", {
         "results": []
+    })
+
+
+def new(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title:str = form.cleaned_data["title"]
+            content:str = form.cleaned_data["content"]
+            entries = util.list_entries()
+            if title.lower() in [entry.lower() for entry in entries]:
+                return render(request, "encyclopedia/new.html", {
+                    "form": form,
+                    "error": True
+                })
+
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse("entry", kwargs={"title": title}))
+
+    return render(request, "encyclopedia/new.html", {
+        "form": NewEntryForm()
     })
